12d3e3ab792217fc6ce66ec1ab5205a9d1d614d7
3jane <nu3jane@gmail.com>
2010-06-04 11:06:41 +0200

    Finished IValueObject<T> interface and Added Documentation folder with the relevant docs (in castillian-spanish) in openoffice format

--- source/dddsample/domain/shared/IValueObject.cs
+++ source/dddsample/domain/shared/IValueObject.cs
@@ -2,6 +2,19 @@ namespace dddsample.domain.shared
 {
     public interface IValueObject<T>
     {
+        /// <summary>
+        /// Value objects compare by the values of their attributes, they don't have an
+        /// identity.
+        /// </summary>
+        /// <param name="the_other">The other value object.</param>
+        /// <returns><code>true</code> if the given value object's and 
+        /// this value object's attributes are the same.</returns>
         bool has_the_same_value_as(T the_other);
+        
+        /// <summary>
+        /// Value objects can be freely copied.
+        /// </summary>
+        /// <returns>A safe, deep copy of this value object.</returns>
+        T copy_into_this();
     }
 }
