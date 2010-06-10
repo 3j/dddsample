1e413f002d79272ff0f44fe63c73c4fea7bb2fdb
3jane <nu3jane@gmail.com>
2010-06-10 19:55:07 +0200

    Changed: CargoSpecs Refactoring.

--- source/dddsample.specs/domain/model/cargo.aggregate/CargoSpecs.cs
+++ source/dddsample.specs/domain/model/cargo.aggregate/CargoSpecs.cs
@@ -7,59 +7,42 @@ namespace dddsample.specs.domain.model.cargo.aggregate
 {
     public abstract class concern_for_cargo : Observes<ICargo, Cargo>
     {
-        //Establish context = () =>
-        //{
-        //    tracking_id = the_dependency<ITrackingId>();
-        //    route_specification = the_dependency<IRouteSpecification>();
-        //};
+        Establish context = () =>
+        {
+            tracking_id = the_dependency<ITrackingId>();
+            route_specification = the_dependency<IRouteSpecification>();
+        };
 
-        //static ITrackingId tracking_id;
-        //static IRouteSpecification route_specification;
+        protected static ITrackingId tracking_id;
+        protected static IRouteSpecification route_specification;
     }
 
     public class when_asked_about_its_tracking_identificator : concern_for_cargo
     {
-        Establish context = () =>
-        {
-            tracking_id = the_dependency<ITrackingId>();
-            route_specification = the_dependency<IRouteSpecification>();
-        };
-
         Because of = () => result = sut.tracking_id();
 
         It should_provide_the_cargo_id = () => result.ShouldEqual(tracking_id);
 
-        static ITrackingId tracking_id;
         static ITrackingId result;
-        static IRouteSpecification route_specification;
     }
 
     public class when_asked_about_its_route_specification : concern_for_cargo
     {
-        Establish context = () =>
-        {
-            tracking_id = the_dependency<ITrackingId>();
-            route_specification = the_dependency<IRouteSpecification>();
-        };
-
         Because of = () => result = sut.route_specification();
 
         It should_provide_the_route_specification = () => result.ShouldEqual(route_specification);
 
         static IRouteSpecification result;
-        static IRouteSpecification route_specification;
-        static ITrackingId tracking_id;
     }
 
     public class when_asked_about_its_origin_location : concern_for_cargo
     {
         Establish context = () =>
         {
-            tracking_id = the_dependency<ITrackingId>();
-            route_specification = the_dependency<IRouteSpecification>();
-
             the_origin_location = an<ILocation>();
-            route_specification.Stub(x => x.origin()).Return(the_origin_location);
+            route_specification
+                .Stub(x => x.origin())
+                .Return(the_origin_location);
         };
 
         Because of = () => result = sut.origin_location();
@@ -72,17 +55,12 @@ namespace dddsample.specs.domain.model.cargo.aggregate
 
         static ILocation the_origin_location;
         static ILocation result;
-        static IRouteSpecification route_specification;
-        static ITrackingId tracking_id;
     }
 
     public class when_comparing_two_equal_cargoes : concern_for_cargo
     {
         Establish context = () =>
         {
-            tracking_id = the_dependency<ITrackingId>();
-            route_specification = the_dependency<IRouteSpecification>();
-
             the_to_compare_cargo = an<ICargo>();
             tracking_id
                 .Stub(x => x.has_the_same_value_as(the_to_compare_cargo.tracking_id()))
@@ -97,19 +75,14 @@ namespace dddsample.specs.domain.model.cargo.aggregate
            tracking_id.received(
               x => x.has_the_same_value_as(the_to_compare_cargo.tracking_id()));
 
-        static ITrackingId tracking_id;
         static ICargo the_to_compare_cargo;
         static bool result;
-        static IRouteSpecification route_specification;
     }
 
     public class when_comparing_two_diferent_cargoes : concern_for_cargo
     {
         Establish context = () =>
         {
-            tracking_id = the_dependency<ITrackingId>();
-            route_specification = the_dependency<IRouteSpecification>();
-
             the_to_compare_cargo = an<ICargo>();
             tracking_id
                 .Stub(x => x.has_the_same_value_as(the_to_compare_cargo.tracking_id()))
@@ -119,10 +92,8 @@ namespace dddsample.specs.domain.model.cargo.aggregate
         Because of = () => result = sut.has_the_same_identity_as(the_to_compare_cargo);
 
         It should_confirm_that_they_have_diferent_identity = () => result.ShouldBeFalse();
-
-        static ITrackingId tracking_id;
+        
         static ICargo the_to_compare_cargo;
         static bool result;
-        static IRouteSpecification route_specification;
     }
 }   
